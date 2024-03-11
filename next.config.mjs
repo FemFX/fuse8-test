8f30393193d86a59fa68c13cc195b2cccf694091
Иван Гнедько <ignedko34@gmail.com>
2024-03-11 22:36:28 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  async exportPathMap(defaultPathMap) {
-    return {
-      "/": { page: "/" },
-    };
-  },
 };
 
 export default nextConfig;
